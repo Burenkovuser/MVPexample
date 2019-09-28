91e1afe998f8b8bdc6783419b1dff94cde9756ed
Burenkovuser <nb2207@yandex.ru>
2019-09-28 13:36:35 +0700

    mvp is ready

---
+++ Test/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  Test
+//
+//  Created by Vasilii on 27/09/2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+}
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+     @IBOutlet var greetingLabel: UILabel!
+    
+    private var person: Person!
+    private var presenter: GreetingViewPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        person = Person(name: "Tom", surname: "Cook")
+        presenter = GreetingPresenter(view: self, preson: person)
     }
 
-
+    @IBAction func showGreetingPressed() {
+        presenter.showGreeting()
+    }
 }
 
+extension ViewController: GreetingViewProtocol {
+    func setGreeting(_ greeting: String) {
+        greetingLabel.text = greeting
+    }
+    
+    
+}
